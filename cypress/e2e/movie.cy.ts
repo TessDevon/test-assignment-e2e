237b5207a1f9f084154766e1dd871e01e3ebf807
Therese <therese.holen22@gmail.com>
2023-02-09 13:01:52 +0100

    /feat Added more tests

--- cypress/e2e/movie.cy.ts
+++ cypress/e2e/movie.cy.ts
@@ -5,38 +5,57 @@ beforeEach(() => {
 });
 
 describe('test for function of search', () => {
-  it('shoud test to open the webpage', () => {
-  });
 
-  it('shoud test when the user fill in a form', () => {
+  it('should test to open the webpage', () => {
+  });
+/*
+  it('should test to open the webpage and do a search at the real API', () => {
+    cy.get("input").should("exist"); 
+    cy.get("input").type("Disney").should("have.value", "Disney");
+    cy.get("button#search").click();
+    cy.get(".movie").should("exist");
+  });
+  */
+
+  it('should test when the user fill in a form', () => {
     cy.intercept("GET", "http://omdbapi.com/?apikey=416ed51a&s=*", {fixture:"example"}).as("movieCall");
     cy.get("input").should("exist"); 
     cy.get("input").type("Dödens grotta").should("have.value", "Dödens grotta");
   });
 
-  it('shoud test when the user fill in a form and press submit', () => {
+  it('should test when the user fill in a form and click submit', () => {
     cy.intercept("GET", "http://omdbapi.com/?apikey=416ed51a&s=*", {fixture:"example"}).as("movieCall");
     cy.get("input").should("exist"); 
     cy.get("input").type("Dödens grotta").should("have.value", "Dödens grotta");
     cy.get("button#search").click();
   });
   
-  it('shoud get mockdata', () => {
+  it('should request with correct url', () => {
     cy.intercept("GET", "http://omdbapi.com/?apikey=416ed51a&s=*", {fixture:"example"}).as("movieCall");
     cy.get("input").should("exist"); 
     cy.get("input").type("Dödens grotta").should("have.value", "Dödens grotta");
     cy.get("button#search").click();
-    cy.get(".movie").should("exist");
+    cy.wait("@movieCall").its("request.url").should("contain", "1")
+  });
+
+  it('should get mockdata', () => {
+    cy.intercept("GET", "http://omdbapi.com/?apikey=416ed51a&s=*", {fixture:"example"}).as("movieCall");
+    cy.get("input").should("exist"); 
+    cy.get("input").type("Dödens grotta").should("have.value", "Dödens grotta");
+    cy.get("button#search").click();
+    cy.wait("@movieCall").get(".movie").should("exist");
   });
 
   it('shoud get mockdata without searchword', () => {
-    cy.intercept("GET", "http://omdbapi.com/?apikey=416ed51a&s=*", {fixture:"exampleEmptyInput"}).as("movieCall");
+    cy.intercept("GET", "http://omdbapi.com/?apikey=416ed51a&s=*", {fixture:"exampleEmptyInput"}).as("movieCallError");
     cy.get("input").should("exist"); 
     //cy.get("input").type("").should("have.value", "");
     cy.get("button#search").click();
-    cy.get("p").should("exist");
+    cy.wait("@movieCallError").get("p").should("exist");
   });
+});
 
+/*
   it('shoud get mockdata with random numbers', () => {
     cy.intercept("GET", "http://omdbapi.com/?apikey=416ed51a&s=*", {fixture:"exampleLongnumberinput"}).as("movieCall");
     cy.get("input").should("exist"); 
@@ -44,10 +63,4 @@ describe('test for function of search', () => {
     cy.get("button#search").click();
     cy.get("p").should("exist");
   });
-
-});
-     //   cy.wait("@movieCall").its("request.url").should("contain","/1");
-    /*
-    cy.get("#movie-container").should("exist");
-    cy.get("h3").contains("Dödens grotta");
-    */
+*/
